eb91cc35ad31d82124f9ad746c446e3e1bb72a0a
Juho Hanhimäki <juho.hanhimaki@arnon.fi>
2018-04-03 14:51:03 +0300

    - Implement AES alternative to windows data protection API to support other operating systems - Update nuget package to release 1.0.0-alpha2

--- ArnonSky.Clients.Device/Security.cs
+++ ArnonSky.Clients.Device/Security.cs
@@ -3,10 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ArnonSky.Clients.Device
 {
@@ -18,8 +20,19 @@ namespace ArnonSky.Clients.Device
         /// <param name="data">Unprotected data</param>
         /// <returns>Protected data</returns>
         public static byte[] ProtectData(byte[] data)
-            =>  ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
-        
+        {
+            try
+            {
+                return ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
+            }
+            catch (PlatformNotSupportedException)
+            {
+                // on linux use aes
+                ReadOrGenerateAesKeys(out var key, out var iv);
+                return AesEncrypt(data, key, iv);
+            }
+        }
+
 
         /// <summary>
         /// Function that may be used to unprotect data under the current user profile
@@ -27,7 +40,99 @@ namespace ArnonSky.Clients.Device
         /// <param name="data">Protected data</param>
         /// <returns>Unprotected data</returns>
         public static byte[] UnprotectData(byte[] data)
-            => ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
+        {
+            try
+            {
+                return ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
+            }
+            catch (PlatformNotSupportedException)
+            {
+                ReadOrGenerateAesKeys(out var key, out var iv);
+                // on linux use aes
+                return AesDecrypt(data, key, iv);
+            }
+        }
+
+        private static void ReadOrGenerateAesKeys(out byte[] key, out byte[] iv)
+        {
+            const string file = ".dataprotector";
+            try
+            {
+                var json = File.ReadAllText(file, Encoding.UTF8);
+                var keys = JsonConvert.DeserializeObject<AesKeys>(json);
+                key = keys.Key;
+                iv = keys.IV;
+            }
+            catch (Exception)
+            {
+                using (var aes = Aes.Create())
+                {
+                    aes.GenerateKey();
+                    aes.GenerateIV();
+                    var keys = new AesKeys() { Key = aes.Key, IV = aes.IV };
+                    File.WriteAllText(file, JsonConvert.SerializeObject(keys), Encoding.UTF8);
+                    key = keys.Key;
+                    iv = keys.IV;
+                }
+            }
+        }
+
+        private static byte[] AesEncrypt(byte[] data, byte[] Key, byte[] IV)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(Convert.ToBase64String(data));
+                        }
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+
+        private static byte[] AesDecrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return Convert.FromBase64String(srDecrypt.ReadToEnd());
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        private class AesKeys
+        {
+            public AesKeys()
+            {
+
+            }
+
+            [JsonProperty(PropertyName = "1")]
+            public byte[] Key { get; set; }
+
+            [JsonProperty(PropertyName = "2")]
+            public byte[] IV { get; set; }
+        }
 
     }
 }
